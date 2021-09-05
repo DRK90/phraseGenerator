e563614838f858317e3148fa00fca7c0e02cfda1
cool-zero <23388426+DRK90@users.noreply.github.com>
2021-09-05 17:01:17 -0500

    add three word banks

---
+++ phraseGenerator.js
@@ -0,0 +1,10 @@
+//javascript
+const nameBank = ["Adam","Bob","Charlie","David","Eric","Frank","Greg","Horace","Isaac","Jimothy",
+"Kevin","Linus","Mark","Nick","Oscar","Plop","Quentin","Roger","Sean","Tim","Ursula","Victor","William",
+"Xerxes","Yannis","Zisko"];
+const verbBank = ["resumes","reproduces with","disagrees with","welcomes", "poses with","laughs at", "regains",
+"smashes the","recovers", "proclaims that the", "examines the", "links to", "screams at", "concedes",
+"rides the", "hits the", "copes with", "bangs the", "prompts the", "spreads the", "dates a", "brushes up against a",
+"transmits to the", "states that", "shows the", "informs the"]
+const nounBank =["psychology", "wife", "profession", "growth", "driver", "inspector", "criticism", "apple", 
+"potato", "relationship"]
